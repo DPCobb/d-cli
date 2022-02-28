9c74d200e61e25c7f4ec99b49fe4e6e62866a2e8
Dan Cobb <dancobb@gmail.com>
2022-02-28 00:47:46 -0500

    [chore]: code comment

--- src/App/IO/Themes/Alt_Printer.php
+++ src/App/IO/Themes/Alt_Printer.php
@@ -4,10 +4,14 @@ namespace App\IO\Themes;
 
 use App\Interfaces\Printer_Interface;
 
+/**
+ * Custom Printer Example, does not work with Static Calls
+ */
 class Alt_Printer implements Printer_Interface
 {
     public function getThemeSettings() : array
     {
+        // Adding entries here allows for additional Output calls.
         return [
             'error' => '0;37;41',
             'warning' => '0;37',
@@ -15,7 +19,8 @@ class Alt_Printer implements Printer_Interface
             'message' => '0;37',
             'info' => '1;31',
             'success' => '0;32',
-            'banner' => '1;37;45'
+            'banner' => '1;37;45',
+            'shout' => '1;37;46' // this would be Output->shout('Some Message');
         ];
     }
 }
