c200f2674ebb52f9f36fb1b9befa852df63b5b62
Dan Cobb <dancobb@gmail.com>
2022-03-05 02:30:28 -0500

    [feat]: basic event handler
    a very basic event handler for triggering actions in a bit cleaner way

--- src/App/Commands/Hello/Default_Handler.php
+++ src/App/Commands/Hello/Default_Handler.php
@@ -3,6 +3,9 @@
 namespace App\Commands\Hello;
 
 use App\Interfaces\Command_Handler_Interface;
+use App\Core\Event_Handler;
+use App\Commands\Hello\Hello_Event;
+use App\IO\Output;
 
 class Default_Handler implements Command_Handler_Interface
 {
@@ -33,9 +36,19 @@ class Default_Handler implements Command_Handler_Interface
      */
     public array $required_parameters = ['name'];
 
+    public function __construct()
+    {
+        $this->ev = new Event_Handler;
+        $this->ev->subscribe('hello-world', new Hello_Event);
+        $this->ev->subscribe('goodbye-world', new Hello_Event);
+        $this->ev->subscribe('goodbye-world', new Hello_Event, 'test');
+    }
+
     public function handle()
     {
-        echo "Hello World";
+        Output::message("Hello World");
+        $this->ev->dispatch('hello-world');
+        $this->ev->dispatch('goodbye-world');
     }
 
     public function world()
---
+++ src/App/Commands/Hello/Hello_Event.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Commands\Hello;
+
+use App\Interfaces\Event_Provider_Interface;
+use App\IO\Output;
+
+class Hello_Event implements Event_Provider_Interface
+{
+    /**
+     * Process the Goodbye event
+     *
+     * @return void
+     */
+    public function goodbyeWorld()
+    {
+        Output::banner('Goodbye!');
+    }
+
+    public function test(string $event)
+    {
+        Output::success('Sent to test from ' . $event);
+    }
+
+    /**
+     * Handles routing the event action
+     *
+     * @param string $event_name
+     *
+     * @return void
+     */
+    public function run(string $event_name)
+    {
+        if (empty($event_name)) {
+            return;
+        }
+        // Parse the event name to a method name
+        $parts = explode('-', $event_name);
+        foreach ($parts as $k => &$v) {
+            if ($k === 0) {
+                continue;
+            }
+            $v = ucwords($v);
+        }
+        $event_name_parsed = implode($parts, '');
+
+        // If we have a method call it
+        if (method_exists($this, $event_name_parsed)) {
+            call_user_func([$this, $event_name_parsed]);
+            return;
+        }
+        
+        // If not process it here...
+        Output::banner($event_name);
+        return;
+    }
+}
